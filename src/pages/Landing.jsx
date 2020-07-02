bba4f93c617c8531dcc83020edba42a44315d9f4
Franklin Liu <FLIU253@uwo.ca>
2020-07-02 15:31:17 -0400

    updated website

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import Header from "../components/header/Header";
-import Layout from "../components/layout/Layout";
-import Section from "../components/section/Section";
-import Link from "../components/link/Link";
+import React from 'react';
+import Header from '../components/header/Header';
+import Layout from '../components/layout/Layout';
+import Section from '../components/section/Section';
+import Link from '../components/link/Link';
 
 const Landing = ({ isDarkTheme, toggleTheme }) => {
   return (
@@ -25,9 +25,9 @@ const Landing = ({ isDarkTheme, toggleTheme }) => {
           link="https://www.autodatasolutions.com/"
           desc="Quality Assurance Automation Analyst Intern, May 2019 - August 2020"
           tasks={[
-            "Automated, modified, executed, debugged and analyzed test results using Cucumber and SerenityBDD to uncover defects in software applications.",
-            "Tracked, identified and logged bugs using Jira, and worked with developers to ensure bugs are fixed, as well as being responsible for automation of features.",
-            "Communicated with product management, development and business analysts in the process of preparation and approval of all test related documents."
+            'Automated, modified, executed, debugged and analyzed test results using Cucumber and SerenityBDD to uncover defects in software applications.',
+            'Tracked, identified and logged bugs using Jira, and worked with developers to ensure bugs are fixed, as well as being responsible for automation of features.',
+            'Communicated with product management, development and business analysts in the process of preparation and approval of all test related documents.',
           ]}
         />
         <Link
@@ -36,9 +36,9 @@ const Landing = ({ isDarkTheme, toggleTheme }) => {
           desc="Instructional Technology Resource Centre Consultant, May 2018 - April 2018"
           tasks={[
             "Assisted professors and faculty members by explaining and teaching them how to use different technologies, including Western's learning management system.",
-            "Created and edited CSS and HTML files for faculty members’ course websites to enhance the students’ learning experience.",
-            "Collaborated with colleagues and faculty members to produce instructional videos using Adobe Photoshop and Adobe Premiere Pro.",
-            "Performed administrative duties, including managing incoming calls, booking appointments, and directing clients to the appropriate technical staff for assistance."
+            'Created and edited CSS and HTML files for faculty members’ course websites to enhance the students’ learning experience.',
+            'Collaborated with colleagues and faculty members to produce instructional videos using Adobe Photoshop and Adobe Premiere Pro.',
+            'Performed administrative duties, including managing incoming calls, booking appointments, and directing clients to the appropriate technical staff for assistance.',
           ]}
         />
       </Section>
@@ -94,7 +94,10 @@ const Landing = ({ isDarkTheme, toggleTheme }) => {
           desc="JavaScript, Python, React, Node.js, Express.js, Vue, Angular, Java, Cucumber, SerenityBDD,C++"
         />
         <Link title="Databases" desc="MongoDB, PostreSQL, MySQL" />
-        <Link title="Others" desc="BDD testing, API design, Agile / Scrum" />
+        <Link
+          title="Others"
+          desc="BDD testing, API design and testing, Agile / Scrum"
+        />
       </Section>
     </Layout>
   );
